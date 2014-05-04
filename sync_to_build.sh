ea788e6ef73c253465a00c5b639fdc36204eaee0
wangdiwen <dw_wang126@126.com>
2014-05-04 11:52:32 +0800

    add some sync script, by diwen

---
+++ sync_to_build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo ''
+echo 'host -> 10.4.89.100'
+echo 'root -> 123456'
+echo ''
+
+expect -c "
+	set timeout 10
+	spawn scp rss/burn_system_60g_min.sh root@10.4.89.100:/root/
+	expect {
+		\"*yes/no\" { send \"yes\n\"; exp_continue; }
+		\"*password\" { send \"123456\n\"; }
+	}
+	expect eof"
+
+expect -c "
+	set timeout 300
+	spawn scp -r rss/burn_data update root@10.4.89.100:/root
+	expect {
+		\"*yes/no\" { send \"yes\n\"; exp_continue; }
+		\"*password\" { send \"123456\n\"; }
+	}
+	expect eof"
+
+echo ''
+echo 'sync ========> ok '
+echo ''
